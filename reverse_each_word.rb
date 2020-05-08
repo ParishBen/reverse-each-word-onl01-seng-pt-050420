0574195b0f9d83cc1aea7fd9f53a4477fed4beb3
Ben Parish <parishben13@gmail.com>
2020-05-08 04:49:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phrase)
   rtrnArray= []
   myArr = []
-  myArr << phrase.split(" ")
+  myArr = phrase.split(" ")
   while myArr
   myArr.each do |word|
    rtrnArray << word.pop
